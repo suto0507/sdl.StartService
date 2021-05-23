6f527194ed5ce69b4b58893e00b9c8632f7238b3
suto0507 <suto0507@gmail.com>
2021-05-23 16:37:42 +0900

    add Service3 and Broadcast Receiver

--- app/src/main/java/jp/ac/titech/itpro/sdl/startservice/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/startservice/MainActivity.java
@@ -1,6 +1,9 @@
 package jp.ac.titech.itpro.sdl.startservice;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,13 +12,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = MainActivity.class.getSimpleName();
+    private BroadcastReceiver scanReceiver;
+    private IntentFilter scanFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate in " + Thread.currentThread());
         setContentView(R.layout.activity_main);
+
+        scanReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                Log.d(TAG, "onReceive: " + action);
+                if (action == null) {
+                    return;
+                }
+                switch (action) {
+                    case Service3.Action_ANSWER:
+                        Log.d(TAG, "myarg = " + intent.getStringExtra(Service3.EXTRA_ANSWER));//受け取ったブロードキャストをログに出力
+                        break;
+                }
+            }
+        };
+        scanFilter = new IntentFilter();
+        scanFilter.addAction(Service3.Action_ANSWER);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        registerReceiver(scanReceiver, scanFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+        unregisterReceiver(scanReceiver);
+    }
+
 
     public void onClickTest1(View v) {
         Log.d(TAG, "onClickTest1 in " + Thread.currentThread());
@@ -28,6 +66,13 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onClickTest2 in " + Thread.currentThread());
         Intent intent = new Intent(this, Service2.class);
         intent.putExtra(Service2.EXTRA_MYARG, "Hello, Service2");
+        startService(intent);
+    }
+
+    public void onClickTest3(View v) {
+        Log.d(TAG, "onClickTest3 in " + Thread.currentThread());
+        Intent intent = new Intent(this, Service3.class);
+        intent.putExtra(Service3.EXTRA_MYARG, "Hello, Service3");
         startService(intent);
     }
 }
---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/startservice/Service3.java
@@ -0,0 +1,48 @@
+package jp.ac.titech.itpro.sdl.startservice;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+public class Service3 extends IntentService {
+    private final static String TAG = Service3.class.getSimpleName();
+    public final static String EXTRA_MYARG = "MYARG";
+    public final static String Action_ANSWER = "Action_ANSWER";
+    public final static String EXTRA_ANSWER = "EXTRA_ANSWER";
+
+    public Service3() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d(TAG, "onHandleIntent in " + Thread.currentThread());
+        Log.d(TAG, "myarg = " + intent.getStringExtra(EXTRA_MYARG));
+
+        //ブロードキャストの送信
+        Intent intent3 = new Intent();
+        intent3.setAction(Action_ANSWER);
+        intent3.putExtra(EXTRA_ANSWER, "Hello, MainActivity. I am Service3.");
+        sendBroadcast(intent3);
+        Log.d(TAG, "Send Broadcast");
+
+
+        try {
+            Thread.sleep(5000); // 5 sec
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate in " + Thread.currentThread());
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy in " + Thread.currentThread());
+        super.onDestroy();
+    }
+}
